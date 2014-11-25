84c954ada30c7429b4c4227939e12b5777466233
Jose Delgado <jose.a.delgado.lopez@gmail.com>
2014-11-24 21:08:44 -0800

    new urll view and template to display lists(redesign)

--- lists/tests.py
+++ lists/tests.py
@@ -60,20 +60,26 @@ class HomePageTest(TestCase):
         response = home_page(request)
         
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
     def test_home_page_only_saves_items_when_necessary(self):
         request = HttpRequest()
         home_page(request)
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_home_page_displays_all_lists_items(self):
+
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+   
+    def test_displays_all_items(self):
         Item.objects.create(text='itemmey 1')
         Item.objects.create(text='itemmey 2')
 
-        request = HttpRequest()
-        response = home_page(request)
-        
-        self.assertIn('itemmey 1', response.content.decode())
-        self.assertIn('itemmey 2', response.content.decode())
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemmey 1')
+        self.assertContains(response, 'itemmey 2')
 
