b432f839000d6454f7fe35eced825dbb78df4d20
Jose Delgado <jose.a.delgado.lopez@gmail.com>
2014-12-23 23:34:33 -0800

    final ch12

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -4,9 +4,8 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from lists.models import Item, List
 from django.utils.html import escape
-from lists.forms import ItemForm
+from lists.forms import ItemForm, DUPLICATE_ITEM_ERROR, EMPTY_LIST_ERROR,ExistingListItemForm
 from lists.views import home_page
-from lists.forms import ItemForm, EMPTY_LIST_ERROR
 from unittest import skip
 
 # Create your tests here.
@@ -54,7 +53,7 @@ class ListViewTest(TestCase):
     def test_disaplys_item_form(self):
         list_ = List.objects.create()
         response = self.client.get('/lists/%d/' % (list_.id,))
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
    
     def post_invalid_input(self):
@@ -72,21 +71,21 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_LIST_ERROR))
     
-    @skip 
     def test_dulicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
         response = self.client.post('/lists/%d/' % (list1.id,), data={'text': 'textey'})
-        expectd_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(),1) 
+
 class NewListTest(TestCase):
 
     def test_saving_a_POST_request(self):
