b07bcc0f7ad22ea31f8529535c233b6a20e3a067
Jose Delgado <jose.a.delgado.lopez@gmail.com>
2015-01-10 11:53:58 -0800

    placeholder test_my_lists and move login checkers into base

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,6 +1,7 @@
 from .base import FunctionalTest
 import time
 from selenium.webdriver.support.ui import WebDriverWait
+TEST_EMAIL = 'edith@mockmyid.com'
 
 class LoginTest(FunctionalTest):
    
@@ -17,7 +18,7 @@ class LoginTest(FunctionalTest):
         ## Use mockmyid.com for test email
         self.browser.find_element_by_id(
             'authentication_email'
-            ).send_keys('edith@mockmyid.com')
+            ).send_keys(TEST_EMAIL)
         self.browser.find_element_by_tag_name('button').click()
         
         # THe Perosona window closes
@@ -39,20 +40,20 @@ class LoginTest(FunctionalTest):
         self.switch_to_new_window('To-Do')
       
         # She can see that she is logged in 
-        self.wait_to_be_logged_in()
+        self.wait_to_be_logged_in(email=TEST_EMAIL)
    
         #Refreshing the page, she sees its a real session login,
         # not just a one-off for that page
         self.browser.refresh()
-        self.wait_to_be_logged_in()
+        self.wait_to_be_logged_in(email=TEST_EMAIL)
  
         #Terrified of this ne feature, she reflectively click "logout"
         self.browser.find_element_by_id('id_logout').click()
-        self.wait_to_be_logged_out()
+        self.wait_to_be_logged_out(email=TEST_EMAIL)
         
         # the "logged out" status also persists after a refresh
         self.browser.refresh()
-        self.wait_to_be_logged_out()
+        self.wait_to_be_logged_out(email=TEST_EMAIL)
 
 
     def switch_to_new_window(self, text_in_title):
@@ -66,21 +67,4 @@ class LoginTest(FunctionalTest):
             time.sleep(0.5)
         self.fail('count not find window')
      
-    def wait_for_element_with_id(self, element_id):
-        WebDriverWait(self.browser, timeout=30).until(
-            lambda b: b.find_element_by_id(element_id),
-            'Could not find element with id {} . Page text was {}'.format(
-                element_id, self.browser.find_element_by_tag_name('body').text
-            )
-        )
-
-    def wait_to_be_logged_in(self):
-        self.wait_for_element_with_id('id_logout')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn('edith@mockmyid.com', navbar.text)
-    
-    def wait_to_be_logged_out(self):
-        self.wait_for_element_with_id('id_login')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertNotIn('edith@mockmyid.com', navbar.text)
 
