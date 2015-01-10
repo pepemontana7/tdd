355da064eb43735ffe5494b0cbdebf188bbff9d7
Jose Delgado <jose.a.delgado.lopez@gmail.com>
2015-01-09 22:43:12 -0800

    custom persona auth backend + custom user model

--- superlists/urls.py
+++ superlists/urls.py
@@ -5,7 +5,5 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'lists.views.home_page', name='home'),
     url(r'^lists/', include('lists.urls')),
-    # url(r'^blog/', include('blog.urls')),
-
-    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/', include('accounts.urls')),
 )
